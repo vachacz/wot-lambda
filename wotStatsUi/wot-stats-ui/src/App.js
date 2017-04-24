a3df4de4736b38dac496dcc5d8ef8037bb55a273
Lukasz Wachowicz <lukasz.wachowicz@mobica.com>
2017-04-24 23:56:30 +0200

    first react navigation implementation

--- wotStatsUi/wot-stats-ui/src/App.js
+++ wotStatsUi/wot-stats-ui/src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { Tab, Tabs, Button, ButtonToolbar } from 'react-bootstrap';
-// import { DatePicker } from 'react-bootstrap-date-picker';
+import { Tab, Tabs, Button, ButtonGroup, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import './App.css';
 
 var DatePicker = require("react-bootstrap-date-picker");
+const EventEmitter = require('events').EventEmitter;
+const emitter = new EventEmitter();
 
-class LoginDetails extends Component {
+class MainNavbar extends Component {
   render() {
     return (
       <div>
-        <b>Account: </b> hawtank <b>Server: </b> EU
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              WoT MyStats
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">hawtank</NavItem>
+              <NavItem eventKey={2} href="#">EU</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
@@ -36,16 +50,28 @@ class MainNavigation extends Component {
 }
 
 class StatPresetSelector extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      preset: ""
+    }
+  }
+  onPresetSelected(preset) {
+    this.setState({
+      preset: preset
+    })
+    emitter.emit('statPresetSelected', preset)
+  }
   render() {
     return (
       <div>
-        Select stats preset:
-        <ButtonToolbar>
-          <Button bsSize="small" bsStyle="primary">Last 10 days</Button>
-          <Button bsSize="small" bsStyle="primary">Last 10 weeks</Button>
-          <Button bsSize="small" bsStyle="primary">Last 10 months</Button>
-          <Button bsSize="small" bsStyle="primary">Custom</Button>
-        </ButtonToolbar>
+        <b>Select stats preset:</b><br/>
+        <ButtonGroup>
+          <Button bsSize="small" bsStyle="success" onClick={this.onPresetSelected.bind(this, '10days')} active={this.state.preset === '10days'}>Last 10 days</Button>
+          <Button bsSize="small" bsStyle="success" onClick={this.onPresetSelected.bind(this, '10weeks')} active={this.state.preset === '10weeks'}>Last 10 weeks</Button>
+          <Button bsSize="small" bsStyle="success" onClick={this.onPresetSelected.bind(this, '10moths')} active={this.state.preset === '10moths'}>Last 10 months</Button>
+          <Button bsSize="small" bsStyle="success">Custom</Button>
+        </ButtonGroup>
       </div>
     );
   }
@@ -63,14 +89,26 @@ class StartDateSelector extends Component {
 }
 
 class DeltaModeSelector extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      deltaMode: ""
+    }
+  }
+  onDeltaModeSelected(deltaMode) {
+    this.setState({
+      deltaMode: deltaMode
+    })
+    emitter.emit('deltaModeSelected', deltaMode)
+  }
   render() {
     return (
       <div>
-        <b>Select delta mode: </b>
-        <ButtonToolbar>
-          <Button bsSize="small" bsStyle="primary">Incremental</Button>
-          <Button bsSize="small" bsStyle="primary">Absolute</Button>
-        </ButtonToolbar>
+        <b>Select delta mode: </b><br/>
+        <ButtonGroup>
+          <Button bsSize="small" bsStyle="success" onClick={this.onDeltaModeSelected.bind(this, 'deltaFromStartDate')} active={this.state.deltaMode === 'deltaFromStartDate'}>Delta from start date</Button>
+          <Button bsSize="small" bsStyle="success" onClick={this.onDeltaModeSelected.bind(this, 'deltaFromLeftDate')} active={this.state.deltaMode === 'deltaFromLeftDate'}>Delta from adjacent date</Button>
+        </ButtonGroup>
       </div>
     );
   }
@@ -91,13 +129,52 @@ class PlayerStatsTab extends Component {
 }
 
 class StatTable extends Component {
+  constructor () {
+    super()
+    this.state = {tableStats: { stats: [] }};
+    let _this = this;
+    emitter.on('statPresetSelected', function(preset) {
+      _this.setState({
+        statPresetSelected: preset,
+        tableStats: { stats: [
+          {stat: 1, damage: 2},
+          {stat: 2, damage: 3}
+        ]}
+      });
+    })
+    emitter.on('deltaModeSelected', function(deltaMode) {
+      _this.setState({
+        deltaModeSelected: deltaMode,
+        tableStats: { stats: [
+          {stat: 1, damage: 2},
+          {stat: 2, damage: 3},
+          {stat: 4, damage: 6}
+        ]}
+      });
+    })
+  }
+  generateHeaderRow() {
+  }
+  generateStatRows() {
+    var data = this.state.tableStats;
+    var rows = data.stats.map(function(stat) {
+      return (
+        <tr>
+          <td> {stat.stat} </td>
+          <td> {stat.damage} </td>
+        </tr>
+      );
+    });
+    return rows;
+  }
   render() {
+    var headerRow = this.generateHeaderRow();
+    var statRows = this.generateStatRows();
     return (
       <div>
         <table>
-          <tr>
-            <td>tabelka</td>
-          </tr>
+          <thead>{headerRow}</thead>
+          <tbody>{statRows}</tbody>
         </table>
       </div>
     );
@@ -108,7 +185,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginDetails />
+        <MainNavbar />
         <MainNavigation />
       </div>
     );
